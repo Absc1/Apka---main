f8734e0e32e296235b8a897606fc3fb4df045b7c
Absc1 <biegasb@gmail.com>
2025-07-18 16:09:16 +0200

    Updated index.ts

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,6 +1,7 @@
 // dodawaj tu każde nowe tło; klucz = nazwa w tagu IMG_<klucz>
 export const IMAGES = {
-  kowal: require('./kowal.jpg'),
-  karczmarka: require('./karczmarka.png'),
+  rastan_angry:      require('./rastan_bad.png'),
+  Rastan_Neutral:  require('./rastan_neutral.png'),
+  rastan_happy:    require('./rastan_happy.png'),
   // kolejne...
 } as const;
