e4c0e93242512a4c45927767d700c0629542116d
Absc1 <biegasb@gmail.com>
2025-07-18 15:42:14 +0200

    Updated index.ts

---
+++ assets/images/index.ts
@@ -0,0 +1,6 @@
+// dodawaj tu każde nowe tło; klucz = nazwa w tagu IMG_<klucz>
+export const IMAGES = {
+  kowal: require('./kowal.jpg'),
+  karczmarka: require('./karczmarka.png'),
+  // kolejne...
+} as const;
